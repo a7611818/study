1fe4d883640b94c8dc38084b4265ee45d62653ec
ybq <869329190@qq.com>
2019-12-25 20:06:11 +0800

    1

--- hello-spring-security-oauth2/hello-spring-security-oauth2-server/src/main/java/com/funtl/spring/security/oauth2/server/configure/WebSecurityConfiguration.java
+++ hello-spring-security-oauth2/hello-spring-security-oauth2-server/src/main/java/com/funtl/spring/security/oauth2/server/configure/WebSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.funtl.spring.security.oauth2.server.configure;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
@@ -15,12 +18,24 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         // 配置默认的加密方式
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsServiceImpl();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // 在内存中创建用户
-        auth.inMemoryAuthentication()
-                .withUser("user").password(passwordEncoder().encode("123456")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder().encode("admin888")).roles("ADMIN");
+
+        auth.userDetailsService(userDetailsService());
     }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        // 在内存中创建用户
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password(passwordEncoder().encode("123456")).roles("USER")
+//                .and()
+//                .withUser("admin").password(passwordEncoder().encode("admin888")).roles("ADMIN");
+//    }
 }
