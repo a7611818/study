dc7e447276f2d889da5684a8f633cbbc06dd26cf
ybq <869329190@qq.com>
2019-12-27 11:55:29 +0800

    1

---
+++ hello-spring-security-oauth2/hello-spring-security-oauth2-resources/src/main/java/com/funtl/spring/security/oauth2/server/OAuth2ResourcesApplication.java
@@ -0,0 +1,16 @@
+package com.funtl.spring.security.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author ybq
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.funtl.spring.security.oauth2.server.mapper")
+public class OAuth2ResourcesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ResourcesApplication.class, args);
+    }
+}
--- hello-spring-security-oauth2/hello-spring-security-oauth2-server/src/main/java/com/funtl/spring/security/oauth2/server/configure/WebSecurityConfiguration.java
+++ hello-spring-security-oauth2/hello-spring-security-oauth2-server/src/main/java/com/funtl/spring/security/oauth2/server/configure/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,9 +20,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/oauth/check_token");
+    }
+
     @Bean
     @Override
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         return new UserDetailsServiceImpl();
     }
 
